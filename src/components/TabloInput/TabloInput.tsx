1b0700a433ceaa1fc1cc88e09da2e8307a215432
aigul <aigul.erma@gmail.com>
2022-01-19 23:48:08 -0400

    first commit

---
+++ src/components/TabloInput/TabloInput.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+type PropsType = {}
+
+export const TabloInput = (props: PropsType) => {
+    return (
+        <div>
+            <h3>Tablo</h3>
+        </div>
+    )
+}
---
+++ src/components/TabloResult/TabloResult.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type PropsType = {
+
+}
+
+export const TabloResult = (props: PropsType) => {
+    return (
+        <div>
+            <h3>Result</h3>
+        </div>
+    )
+}
